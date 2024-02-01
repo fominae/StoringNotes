57a9892cb2fa1376f3305ddfe60439b7a7523f8c
Your Name <your_email@whatever.com>
2024-02-01 22:49:20 +0700

    Добавление метода для перемещения карточек между столбцами

--- js/main.js
+++ js/main.js
@@ -29,5 +29,25 @@ new Vue({
                 this.newCardTitle = '';
                 this.newItemText = '';
             }
-        }}
+        },
+        handleCardPosition(card) {
+            const totalItems = card.items.length;
+            const completedItems = card.items.filter(item => item.completed).length;
+
+            if (completedItems / totalItems > 0.5 && this.column1.includes(card)) {
+                if (this.column2.length ===5 && completedItems / totalItems > 0.5 && this.column1.includes(card)) {
+                    this.disable = false;
+                } else {
+                    this.column1.splice(this.column1.indexOf(card), 1);
+                    this.column2.push(card);
+                    this.saveLocalStorage();
+                }
+            } else if (completedItems / totalItems === 1 && this.column2.includes(card)) {
+                this.column2.splice(this.column2.indexOf(card), 1);
+                this.disable = true;
+                this.column3.push(card);
+                card.completedDate = new Date().toLocaleString();
+                this.saveLocalStorage();
+            }
+        },}
 })
