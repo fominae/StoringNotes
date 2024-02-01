46f2c1fd68e5ac20919f28e16f55ffd99fc162ec
Your Name <your_email@whatever.com>
2024-02-01 22:29:22 +0700

    Создаем метод для добавления карточки

--- js/main.js
+++ js/main.js
@@ -9,4 +9,25 @@ new Vue({
             newItemText: '',
             disable: true
         }
-    },})
+    },
+    methods: {
+        addCard() {
+            if (this.newCardTitle !== '') {
+                const newCard = {
+                    id: Date.now(),
+                    title: this.newCardTitle,
+                    items: this.newItemText.split('\n').filter(item => item.trim() !== '').map(item => ({ text: item, completed: false }))
+                };
+                if (this.newCardTitle !== '' && newCard.items.length >= 3 && newCard.items.length <= 5) {
+                    this.column1.push(newCard);
+                }
+                else alert("Количество пунктов должно быть от 3-х до 5-и!!!")
+                {
+
+                }
+                this.handleCardPosition(newCard);
+                this.newCardTitle = '';
+                this.newItemText = '';
+            }
+        }}
+})
