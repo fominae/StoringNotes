d7975d5b6ceac98ba98c7dc92bc524935decc07a
Your Name <your_email@whatever.com>
2024-02-01 22:19:52 +0700

    Создаем экземпляр Vue

---
+++ js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            column1: [],
+            column2: [],
+            column3: [],
+            newCardTitle: '',
+            newItemText: '',
+            disable: true
+        }
+    },})
